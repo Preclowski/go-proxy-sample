a01706baaa4b732345f66baf1414f29ff8eb72d1
Konrad Pawlikowski <preclowski@gmail.com>
2018-04-29 00:57:24 +0200

    Small refactor

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"github.com/go-redis/redis"
+	"os"
+	"strconv"
+	"log"
 )
 
 type Config struct {
@@ -19,49 +22,19 @@ type Config struct {
 	redisDB       int
 }
 
+var config Config
+var client redis.Client
+
 func getMD5Hash(text string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(text))
+
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	config := Config{
-		"https://www.sample-videos.com/img/Sample-png-image-500kb.png",
-		"http://alfa.kadromierz.pl",
-		"localhost",
-		"6379",
-		"",
-		0,
-	}
-
-	client := redis.NewClient(&redis.Options{
-		Addr:     config.redisHost + ":" + config.redisPort,
-		Password: config.redisPassword,
-		DB:       config.redisDB,
-	})
-
 	fileHash := getMD5Hash(r.RequestURI)
-
-	_, err := client.Get(fileHash).Result()
-
-	if err == redis.Nil {
-		_, err := http.Get(config.backendUrl + r.RequestURI)
-
-		if err != nil {
-			glog.Error("Error on backend")
-			return
-		}
-
-		client.Set(fileHash, nil, 0)
-	}
-
-	resp, err := http.Get(config.s3Url + r.RequestURI)
-
-	if err != nil {
-		glog.Error(err)
-		return
-	}
+	resp := fetchImage(fileHash, r.RequestURI)
 
 	for k, v := range resp.Header {
 		w.Header().Set(k, strings.Join(v, ", "))
@@ -80,7 +53,60 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func existInRedis(fileHash string) bool {
+	_, err := client.Get(fileHash).Result()
+
+	if err == nil {
+		return true
+	}
+
+	return false
+}
+
+func fetchImage(fileHash string, uri string) *http.Response {
+	if existInRedis(fileHash) {
+		resp, err := http.Get(config.backendUrl + uri)
+
+		if err != nil {
+			glog.Error(err)
+		}
+
+		client.Set(fileHash, nil, 0)
+
+		return resp
+	} else {
+		resp, err := http.Get(config.s3Url + uri)
+
+		if err != nil {
+			glog.Error(err)
+		}
+
+		return resp
+	}
+}
+
 func main() {
+	&client = redis.NewClient(&redis.Options{
+		Addr:     config.redisHost + ":" + config.redisPort,
+		Password: config.redisPassword,
+		DB:       config.redisDB,
+	})
+
+	redisDb, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+
+	if err != nil {
+		log.Fatal("Invalid redis DB")
+	}
+
+	config = Config{
+		os.Getenv("S3_URL"),
+		os.Getenv("BACKEND_URL"),
+		os.Getenv("REDIS_HOST"),
+		os.Getenv("REDIS_PORT"),
+		os.Getenv("REDIS_PASSWORD"),
+		redisDb,
+	}
+
 	http.HandleFunc("/", handle)
 	http.ListenAndServe(":8080", nil)
 }
